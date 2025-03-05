11b95712875f690ef61b638f4a8dce510862402b
terrytaylorbonn <terrytaylorbonn@gmail.com>
2025-03-05 07:59:07 -0500

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// filepath: /c:/Users/terry/Downloads/json-server/server.js
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3002;
-
+server.use(cors()); // Add this line to enable CORS
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
